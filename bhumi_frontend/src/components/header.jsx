36851f75b545fb3a9504c0ff93358d84ab8b96e6
Bibek Pandey <pandeybibe098k@gmail.com>
2025-02-16 22:00:14 +0545

    updated header of userdashboard

--- bhumi_frontend/src/components/header.jsx
+++ bhumi_frontend/src/components/header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                 <p className="mt-4 text-gray-600">
                     Buy Fresh, Support Farmers, and Embrace Sustainable Living
                 </p>
-                <button className="mt-6 px-6 py-3 bg-green-700 text-white rounded-lg hover:bg-green-800">
+                <button href="/product" className="mt-6 px-6 py-3 bg-green-700 text-white rounded-lg hover:bg-green-800">
                     View Product
                 </button>
             </div>
